143ce1256e0d4c8e7c120a2fc7b1b022ff9be243
Anissa Lamh <anissa.lamhaouar@gmail.com>
2019-12-19 11:57:28 +0100

    Add models : Grid, Activity, Parameters

---
+++ Models/Activity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace humanlab.Models
+{
+    class Activity
+    {
+        public int IdActivity { get; set; }
+        public string ActivityName { get; set; }
+        public int FixingTime { get; set; }
+
+    }
+}
---
+++ Models/Grid.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace humanlab.Models
+{
+    class Grid
+    {
+        public int IdGrid { get; set; }
+        public string GridName { get; set; }
+        public int ElementsSize { get; set; }
+
+    }
+}
