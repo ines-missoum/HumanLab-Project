756d3e23b24db0abc5e4d04c32e1a6d0037f32a1
Anissa Lamh <anissa.lamhaouar@gmail.com>
2019-12-19 11:59:09 +0100

    Add Entity Joining models:  ActivityGrids, GridElements

---
+++ Models/ActivityGrids.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace humanlab.Models
+{
+    class ActivityGrids
+    {
+        public int ActivityId { get; set; }
+        public Activity Activity { get; set; }
+
+        public int GridId { get; set; }
+        public Grid Grid { get; set; }
+    }
+}
