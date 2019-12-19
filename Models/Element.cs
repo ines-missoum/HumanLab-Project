1212f1fb3fbdb788651094fd5628df245f5ba363
Anissa Lamh <anissa.lamhaouar@gmail.com>
2019-12-19 11:28:38 +0100

    Add model : Element Model

---
+++ Models/Element.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace humanlab.Models
+{
+    class Element
+    {
+        public int IdElement { get; set; }
+        public string ElementName { get; set; }
+        public string Image { get; set; }
+        public string Audio { get; set; }
+        public string SpeachText { get; set; }
+
+    }
+}
