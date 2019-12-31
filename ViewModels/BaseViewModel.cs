1298d397b6d48a8cd40a2aada10af121c50981b1
ines-missoum <32544041+ines-missoum@users.noreply.github.com>
2019-12-31 18:48:47 +0100

    done view model on navigation

---
+++ ViewModels/BaseViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+namespace humanlab.ViewModels
+{
+    /// <summary>
+    /// Cette classe représente l'emetteur d'evenement sur les changements de propriétés. Elle va être utilisée comme base de tous les ViewModel
+    /// </summary>
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ ViewModels/NavigationViewModel.cs
@@ -0,0 +1,92 @@
+﻿using humanlab.Views;
+using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace humanlab.ViewModels
+{
+    public class NavigationViewModel : BaseViewModel
+    {
+        public NavigationViewModel()
+        {
+            contentFrame = typeof(AllActivitiesView);
+            title = "Accueil";
+        }
+
+        public Type contentFrame;
+        public String title;
+
+        public String Title
+        {
+            get => title;
+            set
+            {
+                if (value != title)
+                {
+                    title = value;
+                    OnPropertyChanged("Title");
+                }
+            }
+        }
+
+        public Type ContentFrame
+        {
+            get => contentFrame;
+            set
+            {
+                if (value != contentFrame)
+                {
+                    contentFrame = value;
+                    OnPropertyChanged("ContentFrame");
+                }
+            }
+        }
+
+
+        public void nvTopLevelNav_Loaded(object sender, RoutedEventArgs e)
+        {
+        }
+
+        public void nvTopLevelNav_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            // we use itemInvoked instead
+        }
+
+        public void nvTopLevelNav_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+
+            if (args.IsSettingsInvoked)
+            {
+                this.Title = "Paramètres";
+                this.ContentFrame = typeof(SettingsView);
+            }
+            else
+            {
+                string ItemContent = args.InvokedItem as string;
+
+                if (ItemContent != null)
+                {
+                    this.Title = ItemContent;
+                    switch (ItemContent)
+                    {
+                        
+                        case "Toutes les activités":
+                            this.ContentFrame = typeof(AllActivitiesView);
+                            break;
+                        case "Nouvelle activité":
+                            break;
+                        case "Toutes les grilles":
+                            break;
+                        case "Nouvelle grille":
+                            break;
+                        case "Tous les éléments":
+                            break;
+                        case "Nouvel élément":
+                            break;
+                    }
+                }
+
+            }
+        }
+    }
+}
