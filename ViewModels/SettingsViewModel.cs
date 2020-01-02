708b16c2c94e8b905cc95aa88a0e824fb1efbeb0
ines-missoum <32544041+ines-missoum@users.noreply.github.com>
2020-01-02 12:06:26 +0100

    add view model on settings

---
+++ ViewModels/SettingsViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace humanlab.ViewModels
+{
+    class SettingsViewModel : BaseViewModel
+    {
+        private bool isManualChecked = false;
+        private bool isAutoChecked = false;
+
+        public SettingsViewModel()
+        {
+            this.isManualChecked = false;
+            this.isAutoChecked = false;
+        }
+
+        public bool IsManualChecked
+        {
+            get => isManualChecked;
+            set
+            {
+                if (value != isManualChecked)
+                {
+                    isManualChecked = value;
+                    OnPropertyChanged("IsManualChecked");
+                }
+            }
+        }
+
+        public bool IsAutoChecked
+        {
+            get => isAutoChecked;
+            set
+            {
+                if (value != isAutoChecked)
+                {
+                    isAutoChecked = value;
+                    OnPropertyChanged("IsAutoChecked");
+                }
+            }
+        }
+    }
+}
