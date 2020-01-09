6fd405bbb980a362861e6171272194662c633c4c
ines-missoum <inesmiss@live.fr>
2020-01-09 17:24:59 +0100

    wip creation grid pop up

---
+++ ViewModels/GridFormViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace humanlab.ViewModels
+{
+    public class GridFormViewModel
+    {
+        public GridFormViewModel()
+        {
+            SearchedElements = new List<String>();
+            SearchedElements.Add("hello");
+            SearchedElements.Add("salut");
+            SearchedElements.Add("salut");
+            SearchedElements.Add("salut");
+            SearchedElements.Add("salut");
+            SearchedElements.Add("salut");
+            SearchedElements.Add("salut");
+            SearchedElements.Add("salut");
+            SearchedElements.Add("salut");
+            SearchedElements.Add("hello");
+            SearchedElements.Add("salut");
+            SearchedElements.Add("salut");
+            SearchedElements.Add("salut");
+            SearchedElements.Add("salut");
+            SearchedElements.Add("salut");
+            SearchedElements.Add("salut");
+            SearchedElements.Add("salut");
+            SearchedElements.Add("salut");
+            SearchedElements.Add("hello");
+            SearchedElements.Add("salut");
+            SearchedElements.Add("salut");
+            SearchedElements.Add("salut");
+            SearchedElements.Add("salut");
+            SearchedElements.Add("salut");
+            SearchedElements.Add("salut");
+            SearchedElements.Add("salut");
+            SearchedElements.Add("salut");
+            SearchedElements.Add("hello");
+            SearchedElements.Add("salut");
+            SearchedElements.Add("salut");
+            SearchedElements.Add("salut");
+            SearchedElements.Add("salut");
+            SearchedElements.Add("salut");
+            SearchedElements.Add("salut");
+            SearchedElements.Add("salut");
+            SearchedElements.Add("salut");
+        }
+        public List<String> SearchedElements { get; set; }
+    }
+}
