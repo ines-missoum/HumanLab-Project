71205b99cb1050701e0edf57d9600fdc7b79e047
ines-missoum <inesmiss@live.fr>
2020-01-20 18:35:27 +0100

    fix comments added

--- ViewModels/GridFormViewModel.cs
+++ ViewModels/GridFormViewModel.cs
@@ -15,89 +15,104 @@ namespace humanlab.ViewModels
 {
     public class GridFormViewModel : BaseViewModel
     {
-        /*public attributes*/
+        /*** PUBLIC ATTRIBUTES ***/
+
+        private List<ElementChecked> allElements;
         private List<ElementChecked> searchedElements;
         private List<ElementChecked> selectedElements;
-        private List<ElementChecked> allElements;
-        private bool isChooseElementsOpened;
-        private bool isOrganizeElementsOpened;
-        private bool isNextButtonShowing;
-        public DelegateCommand ChoosePopUpVisibility { get; set; }
-        public DelegateCommand OrganizePopUpVisibility { get; set; }
-        private string buttonText;
         private List<string> categories;
+
+        //attributes of choosing elements view
+        private bool isChooseElementsOpened;
+        public DelegateCommand ChoosePopUpVisibility { get; set; }
+
+        //attributes of organize elements view
+        private bool isOrganizeElementsOpened;
+        public DelegateCommand OrganizePopUpVisibility { get; set; }
+
+        //attributes linked to dynamic messages in front
+        private bool isNextButtonShowing;
+        private string buttonText;
         private bool isEmptySearchMessageShowing;
         private bool isEmptyElementMessageShowing;
-        private string gridName;
 
-        /*private attributes*/
+        /*** PRIVATE ATTRIBUTES ***/
+
+        // attributes linked to the management of the selection in the choose elements view :
+
+        /// <summary>
+        /// Allows to know if a research is happening in the choose elements view
+        /// </summary>
         private bool searching;
+
+        /// <summary>
+        /// Allows to know if the selection has just changed in the first grid view
+        /// </summary>
         private bool selectionChangedFirstGridView;
+
+        /// <summary>
+        /// First grid view => contains the elements that are resulting of the search
+        /// </summary>
         private GridView searchedGridView;
-        private Repository repository;
+
+        //values of the form 
         private string searchText { get; set; }
         private string searchCategory { get; set; }
+        private string gridName;
+
+        /// <summary>
+        /// Responsible for looking in database
+        /// </summary>
+        private Repository repository;
+
+        /***CONSTRUCTOR***/
 
         public GridFormViewModel()
         {
             repository = new Repository();
-            InitialiseAllElements();
+
+            //initialisation of al the lists
+            InitialiseAllElementsAndCategories();
             SearchedElements = new List<ElementChecked>(AllElements.OrderByDescending(e => e.Element.ElementName.Length));
             SelectedElements = new List<ElementChecked>();
-            searching = false;
-            //pop up closed at the beginning
+
+            //the views are not displayed at the the beginning
             isChooseElementsOpened = false;
             isOrganizeElementsOpened = false;
             ChoosePopUpVisibility = new DelegateCommand(ChangeChoosePopUpVisibility);
             OrganizePopUpVisibility = new DelegateCommand(OpenOrganizationPopUpIfAllowed);
-            ButtonText = "Choisir";
+
+            //no search has began
+            searching = false;
+
             IsNextButtonShowing = false;
-            searchText = "";
-            searchCategory = "Tout";
             isEmptySearchMessageShowing = false;
             isEmptyElementMessageShowing = true;
+            ButtonText = "Choisir";
+
+            //form default values
+            searchCategory = "Tout";
+            searchText = "";
             gridName = "";
         }
 
-        private void ChangeChoosePopUpVisibility()
+        /// <summary>
+        /// Method that retrieve all the elements from the database and save them as ElementChecked (that has in addition a IsSelected attribute to handle the selection in the grid view)
+        /// It also retrieve all the categories to set the combo box in the view for the search
+        /// </summary>
+        private async void InitialiseAllElementsAndCategories()
         {
-            IsChooseElementsOpened = !IsChooseElementsOpened;
-        }
-        private async void OpenOrganizationPopUpIfAllowed()
-        {
-            string errorMessage = null;
-
-            if (gridName.Equals(""))
-                errorMessage = "Veuillez entrer un nom de grille pour poursuivre.";
-            else
-            {
-                //we check if the name is not already taken
-                List<string> gridsNames = await repository.GetGridsNamesAsync();
-                if(gridsNames.Contains(gridName))
-                    errorMessage = "Une grille porte déjà le nom que vous avez choisi. Veuillez le modifier pour poursuivre.";
-            }
-            
-            //we show error if there is one
-            if (errorMessage != null)
-            {
-                MessageDialog messageDialog = new MessageDialog(errorMessage);
-                // display the message dialog with the proper error 
-                await messageDialog.ShowAsync();
-            }
-            else
-                //else we display the organization view
-                IsOrganizeElementsOpened = !IsOrganizeElementsOpened;
-        }
-
-        private async void InitialiseAllElements()
-        {
+            //retrieve all the elements
             var elements = await repository.GetElementsAsync();
             AllElements = new List<ElementChecked>();
             elements.ForEach(e => AllElements.Add(new ElementChecked(e, false)));
+
+            //retrieve all categories
             Categories = elements.Select(e => e.Category.CategoryName).Distinct().ToList();
             Categories.Add("Tout");
         }
 
+        /*** GETTERS AND SETTERS FOR PUBLIC ATTRIBUTES ***/
         public string ButtonText
         {
             get => buttonText;
@@ -231,6 +246,50 @@ namespace humanlab.ViewModels
             }
         }
 
+        /// <summary>
+        /// Change the visibility of the choose elements view
+        /// </summary>
+        private void ChangeChoosePopUpVisibility()
+        {
+            IsChooseElementsOpened = !IsChooseElementsOpened;
+        }
+
+        /// <summary>
+        /// Check if the gridName is not empty are already existing. If so, display an error message, else open the organization view
+        /// </summary>
+        private async void OpenOrganizationPopUpIfAllowed()
+        {
+            string errorMessage = null;
+
+            if (gridName.Equals(""))
+                errorMessage = "Veuillez entrer un nom de grille pour poursuivre.";
+            else
+            {
+                //we check if the name is not already taken
+                List<string> gridsNames = await repository.GetGridsNamesAsync();
+                if (gridsNames.Contains(gridName))
+                    errorMessage = "Une grille porte déjà le nom que vous avez choisi. Veuillez le modifier pour poursuivre.";
+            }
+
+            //we show error if there is one
+            if (errorMessage != null)
+            {
+                MessageDialog messageDialog = new MessageDialog(errorMessage);
+                // display the message dialog with the proper error 
+                await messageDialog.ShowAsync();
+            }
+            else
+                //else we display the organization view
+                IsOrganizeElementsOpened = !IsOrganizeElementsOpened;
+        }
+
+        /*** METHODS THAT DEALS WITH GRIDVIEW SELECTION ISSUES IN THE CHOOSE ELEMENT VIEW***/
+
+        /// <summary>
+        /// Method called at the loading time. Pre select all elements that have the isSelect attributes to true.
+        /// </summary>
+        /// <param name="sender">the first grid view</param>
+        /// <param name="args">arguments</param>
         public void GridView1_Loading(FrameworkElement sender, object args)
         {
             GridView gridview1 = sender as GridView;
@@ -238,6 +297,13 @@ namespace humanlab.ViewModels
             RefreshSelectionSearchedGrid();
         }
 
+        /// <summary>
+        /// Method called when the selection change in the first grid view.
+        /// If an element is selected, it is added in the selected elements list.
+        /// If an element is unselected, it is removed in the selected elements list.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         public void GridView1_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             selectionChangedFirstGridView = true;
@@ -271,12 +337,23 @@ namespace humanlab.ViewModels
             selectionChangedFirstGridView = false;
         }
 
+        /// <summary>
+        /// Method called at the loading time. Pre select all elements of the list.
+        /// </summary>
+        /// <param name="sender">the second grid view</param>
+        /// <param name="args">arguments</param>
         public void GridView2_Loading(FrameworkElement sender, object args)
         {
             GridView gridview2 = sender as GridView;
             gridview2.SelectAll();
         }
 
+        /// <summary>
+        /// Method called when an element of the second gridview is unselect.
+        /// We remove it from the slected elements list and unselect it from the search list
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         public void GridView2_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             GridView gridview = sender as GridView;
@@ -287,8 +364,7 @@ namespace humanlab.ViewModels
                 if (SelectedElements.Contains(removedItem))
                     searchedGridView.SelectedItems.Remove(removedItem);
             }
-
-
+            //because all the elements are automatically unselected
             gridview.SelectAll();
         }
 
@@ -298,6 +374,9 @@ namespace humanlab.ViewModels
             gridview.SelectAll();
         }
 
+        /// <summary>
+        /// set all elements that have the isSelected attribute at true as selected in the first gridView.
+        /// </summary>
         private void RefreshSelectionSearchedGrid()
         {
             foreach (ElementChecked item in SearchedElements)
@@ -309,6 +388,13 @@ namespace humanlab.ViewModels
             }
         }
 
+        /*** FORM HANDLING METHODS ***/
+
+        /// <summary>
+        /// Method called at each changes in the text box og the gridName. We save the new value.
+        /// </summary>
+        /// <param name="sender"> gridName textbox</param>
+        /// <param name="e">arguments</param>
         public void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
             TextBox control = sender as TextBox;
@@ -316,8 +402,13 @@ namespace humanlab.ViewModels
             gridName = name;
         }
 
-        //search methods
+        /**Search hanling methods**/
 
+        /// <summary>
+        /// Retrieve the name searched, save it and run the search.
+        /// </summary>
+        /// <param name="sender">search by name field</param>
+        /// <param name="args">arguments </param>
         public void AutoSuggestBox_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
         {
             searching = true;
@@ -326,6 +417,11 @@ namespace humanlab.ViewModels
             searching = false;
         }
 
+        /// <summary>
+        /// Retrieve the name searched, save it and run the search.
+        /// </summary>
+        /// <param name="sender">search by name field</param>
+        /// <param name="args">arguments </param>
         public void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs args)
         {
             searching = true;
@@ -336,14 +432,19 @@ namespace humanlab.ViewModels
             searching = false;
         }
 
+        /// <summary>
+        /// Update the searchedElements list taking care of the category and name searched.
+        /// </summary>
         private void Search()
         {
             List<ElementChecked> newSearchedCat = new List<ElementChecked>(AllElements);
             if (searchCategory.Equals("Tout"))
+                //then we search only by names
                 SearchedElements = newSearchedCat.Where(e => e.Element.ElementName.Contains(searchText))
                                                .OrderByDescending(e => e.Element.ElementName.Length)
                                                .ToList();
             else
+                //else we search by name (not strict => just checking if the name contains the search) AND by category
                 SearchedElements = newSearchedCat.Where(e => e.Element.Category.CategoryName.Equals(searchCategory) && e.Element.ElementName.Contains(searchText))
                                                    .OrderByDescending(e => e.Element.ElementName.Length)
                                                    .ToList();
@@ -351,7 +452,6 @@ namespace humanlab.ViewModels
             IsEmptySearchMessageShowing = SearchedElements.Count() == 0;
             RefreshSelectionSearchedGrid();
         }
-
 
     }
 }
